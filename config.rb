cb9b9123b936129b363d0f51658f4baa232e9116
Rick <ricksterhd123@gmail.com>
2024-01-27 06:36:51 +0000

    another milestone: add fizzbuzz socket example driven by mruby event handler

--- config.rb
+++ config.rb
@@ -1,5 +1,21 @@
 puts $mruv
 
+##
+# mruv socket handler function fizzbuzz example
+#
 def handler(env)
-  "Hello " + env
+  n = env.chomp.to_i
+  if n > 0 and n < 100 then
+    if n % 15 == 0 then
+      "fizzbuzz"
+    elsif n % 3 == 0 then
+      "fizz"
+    elsif n % 5 == 0 then
+      "buzz"
+    else
+      env
+    end
+  else
+    "Must be number between 1 and 99"
+  end
 end
--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <uv.h>
+
 #include <mruby.h>
 #include <mruby/compile.h>
 #include <mruby/proc.h>
@@ -43,7 +45,7 @@ void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
     buf->len = suggested_size;
 }
 
-void echo_write(uv_write_t *req, int status)
+void on_write_complete(uv_write_t *req, int status)
 {
     if (status)
     {
@@ -52,25 +54,34 @@ void echo_write(uv_write_t *req, int status)
     free_write_req(req);
 }
 
-void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
+void on_read_chunk(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
 {
-    if (nread > 0)
-    {
-        write_req_t *req = (write_req_t *)malloc(sizeof(write_req_t));
-        req->buf = uv_buf_init(buf->base, nread);
-
-        uv_write((uv_write_t *)req, client, &req->buf, 1, echo_write);
-        return;
-    }
-
     if (nread < 0)
     {
         if (nread != UV_EOF)
+        {
             fprintf(stderr, "Read error %s\n", uv_err_name(nread));
-        uv_close((uv_handle_t *)client, NULL);
+            uv_close((uv_handle_t *)client, NULL);
+        }
+        free(buf->base);
     }
+    else if (nread > 0)
+    {
+        write_req_t *req = (write_req_t *)malloc(sizeof(write_req_t));
 
-    free(buf->base);
+        mrb_value ret = mrb_funcall(mrb, config_script, "handler", (mrb_int)1, mrb_str_new_cstr(mrb, buf->base));
+        mrb_value ret_as_str = mrb_obj_as_string(mrb, ret);
+        const char *ret_as_cstr_tmp = mrb_string_value_cstr(mrb, &ret_as_str);
+
+        // NOTE: ret_as_cstr is passed into uv_buf_init freed by libuv on complete
+        ssize_t ret_as_cstr_sz = sizeof(char) * (strlen(ret_as_cstr_tmp) + 2);
+        char *ret_as_cstr = (char *)malloc(ret_as_cstr_sz);
+        snprintf(ret_as_cstr, ret_as_cstr_sz, "%s\n", ret_as_cstr_tmp);
+
+        req->buf = uv_buf_init(ret_as_cstr, ret_as_cstr_sz);
+        uv_write((uv_write_t *)req, client, &req->buf, 1, on_write_complete);
+        return;
+    }
 }
 
 void on_new_connection(uv_stream_t *server, int status)
@@ -86,7 +97,7 @@ void on_new_connection(uv_stream_t *server, int status)
 
     if (uv_accept(server, (uv_stream_t *)client) == 0)
     {
-        uv_read_start((uv_stream_t *)client, alloc_buffer, echo_read);
+        uv_read_start((uv_stream_t *)client, alloc_buffer, on_read_chunk);
     }
     else
     {
@@ -94,44 +105,20 @@ void on_new_connection(uv_stream_t *server, int status)
     }
 }
 
-// mrb_value add_event_handler(mrb_state *mrb, mrb_value self)
-// {
-//     const char *event_name;
-//     mrb_value blk;
-
-//     mrb_get_args(mrb, "&", &blk);
-//     if (mrb_nil_p(blk))
-//     {
-//         mrb_raise(mrb, E_ARGUMENT_ERROR, "tried to add event handler without a block");
-//     }
-
-//     struct RProc *p = mrb_proc_ptr(blk);
-//     if (!MRB_PROC_STRICT_P(p))
-//     {
-//         struct RProc *p2 = MRB_OBJ_ALLOC(mrb, MRB_TT_PROC, p->c);
-//         mrb_proc_copy(mrb, p2, p);
-//         p2->flags |= MRB_PROC_STRICT;
-//         return mrb_obj_value(p2);
-//     }
-
-//     MRB_METHOD_FROM_PROC(mbr, p);
-//     return mrb_vm_run(mrb, p, self, 0);
-// }
-
 int main()
 {
     int error = 0;
+
     // Load config.rb file
     FILE *fp = fopen("config.rb", "r");
-
     if (!fp)
     {
         fprintf(stderr, "Failed to open `config.rb`\n");
         return 1;
     }
 
+    // Setup mrb_state
     mrb = mrb_open();
-
     if (!mrb)
     {
         fprintf(stderr, "Failed to open mrb\n");
@@ -146,23 +133,24 @@ int main()
     // Load file
     config_script = mrb_load_file(mrb, fp);
 
-    if (mrb->exc) {
+    if (mrb->exc)
+    {
         error = 1;
         goto CLEANUP;
     }
 
     // Check handler exists
     mrb_method_t handler = mrb_method_search_vm(mrb, &mrb->object_class, mrb_intern_lit(mrb, "handler"));
-    if (!handler) {
+    if (!handler)
+    {
         fprintf(stderr, "`handler` method does not exist\n");
         error = 1;
         goto CLEANUP;
     }
 
     // Call the handler
-    mrb_value ret = mrb_funcall(mrb, config_script, "handler", (mrb_int) 1, mrb_str_new_cstr(mrb, "world!"));
+    mrb_value ret = mrb_funcall(mrb, config_script, "handler", (mrb_int)1, mrb_str_new_cstr(mrb, "world!"));
     mrb_value ret_as_string = mrb_obj_as_string(mrb, ret);
-    printf("%s\n", mrb_string_value_cstr(mrb, &ret_as_string));
 
     // Run event loop
     loop = uv_default_loop();
